1971d8d81ffd2589feb3c3c3b72acfc3e69f2186
onurkaymak <onurkaymak@outlook.com>
2023-12-07 11:54:16 -0800

    Pass test #2 for top-stories-reducer.

--- src/__tests__/reducers/top-stories-reducer.test.js
+++ src/__tests__/reducers/top-stories-reducer.test.js
@@ -1,6 +1,8 @@
 import topStoriesReducer from '../../reducers/top-stories-reducer';
+import * as c from './../../actions/ActionTypes';
 
-describe('#1 topStoriesReducer', () => {
+describe('topStoriesReducer', () => {
+  let action;
 
   const initialState = {
     isLoaded: false,
@@ -8,11 +10,28 @@ describe('#1 topStoriesReducer', () => {
     error: null
   };
 
-  test('should successfully throw a new error if a non-matching action type is passed into it', () => {
+  test('#1 Should successfully throw a new error if a non-matching action type is passed into it', () => {
     expect(
       () => {
         topStoriesReducer(initialState, { type: null })
       }
     ).toThrowError("There is no action matching null.");
   });
+
+  test('#2 Successfully getting top stories should change isLoaded to true and update topStories', () => {
+
+    const topStories = "An article";
+
+    action = {
+      type: c.GET_TOP_STORIES_SUCCESS,
+      topStories
+    };
+
+    expect(topStoriesReducer(initialState, action)).toEqual({
+      isLoaded: true,
+      topStories: "An article",
+      error: null
+    });
+  });
+
 });
