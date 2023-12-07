7a30e34bf913d701df74d69b9e37b8810528ac0f
onurkaymak <onurkaymak@outlook.com>
2023-12-07 10:00:52 -0800

    Add state management in TopStories component.

--- src/components/TopStories.js
+++ src/components/TopStories.js
@@ -1,5 +1,11 @@
+import React, { useState } from 'react';
 
 const TopStories = () => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [topStories, setTopStories] = useState([]);
+
+
   return (
     <h1>Top Stories</h1>
   )
